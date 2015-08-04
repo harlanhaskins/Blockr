ecfd164683cd0795992a5516169baf457cbf3c9d
Harlan Haskins <harlan@harlanhaskins.com>
2015-08-04 17:20:57 -0400

    Initial Commit

---
+++ Blockr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blockr
+//
+//  Created by Harlan Haskins on 8/4/15.
+//  Copyright © 2015 Harlan Haskins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BlockrUITests/BlockrUITests.swift
@@ -0,0 +1,34 @@
+//
+//  BlockrUITests.swift
+//  BlockrUITests
+//
+//  Created by Harlan Haskins on 8/4/15.
+//  Copyright © 2015 Harlan Haskins. All rights reserved.
+//
+
+import XCTest
+
+class BlockrUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
